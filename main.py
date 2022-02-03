7731ede7c68a1c6c786a1906fb68153defeea929
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2022-02-04 00:10:45 +0500

    work

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-import tkinter as tk
+import win32com.client as comclt
+from twitchio.ext import commands
+
+wsh = comclt.Dispatch("WScript.Shell")
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(token='...', prefix='!', initial_channels=['...'])
+
+    async def event_ready(self):
+        print(f'Logged in as | {self.nick}')
+
+    @commands.command()
+    async def tv(self, ctx: commands.Context):
+        wsh.AppActivate("TWITCH STUDIO")
+        wsh.SendKeys("^K")
+        await ctx.send(f'{ctx.author.name} вывел видео на весь экран')
+
+    @commands.command()
+    async def back(self, ctx: commands.Context):
+        wsh.AppActivate("TWITCH STUDIO")
+        wsh.SendKeys("^L")
+        await ctx.send(f'{ctx.author.name} вернул обратно в общий режим')
+
+
+
+
+
+bot = Bot()
+bot.run()
+
 
